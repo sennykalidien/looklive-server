52782ea7f59cdb401d781c61f2eebe6de78d475c
Senny Kalidien <senny@directzichtbaar.nl>
2016-03-12 16:36:41 +0100

    EXTRA: Critical CSS + Gulp
    Free time in the weekend === Adding Critical CSS & a start to making a
    Gulp Pipeline.
    
    NOTE TO TEACHERS: I know I’m late with this extra assignments, so this
    doesn’t need to be graded. Just doing it for my own pleasure. Knowledge
    is power…

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,56 @@
 var gulp = require('gulp'),
-    svgSprite = require('gulp-svg-sprite');
+    svgSprite = require('gulp-svg-sprite'),
+    critical = require('critical');
 
-var svgConfig = {
-    dest: '.',
-    shape: {
-        dimension: {
-            maxWidth: 15,
-            maxHeight: 15
-        },
-        spacing: {
-            padding: 5,
-        },
-    },
-    mode: {
-        css: {
-            dest: '.',
-            sprite: 'icns',
-            render: {
-                css: true
+    var svgConfig = {
+        dest: '.',
+        shape: {
+            dimension: {
+                maxWidth: 15,
+                maxHeight: 15
             },
-            example: true,
-            prefix: '.icn-'
+            spacing: {
+                padding: 5,
+            },
+        },
+        mode: {
+            css: {
+                dest: '.',
+                sprite: 'icns',
+                render: {
+                    css: true
+                },
+                example: true,
+                prefix: '.icn-'
+            }
         }
-    }
-};
+    };
 
 gulp.task('icons', function () {
-    gulp.src('./public/icons/svg/*.svg')
+    gulp.src('./public/images/icons/svg/*.svg')
         .pipe(svgSprite(svgConfig))
-        .pipe(gulp.dest('./public/icons/'));
+        .pipe(gulp.dest('./public/images/icons/'));
+});
+
+
+gulp.task('critical', function (cb) { //src: http://fourkitchens.com/blog/article/use-gulp-automate-your-critical-path-css
+    critical.generate({
+        base: './',
+        src: 'public/home.html',
+        css: ['./public/css/style.css'],
+        dimensions: [{
+            width: 320,
+            height: 480
+    }, {
+            width: 768,
+            height: 1024
+    }, {
+            width: 1280,
+            height: 960
+    }],
+        dest: './public/css/critical.css',
+        minify: true,
+        extract: false
+        //ignore: ['font-face']
+    });
 });
